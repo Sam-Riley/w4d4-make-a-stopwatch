121179635d9280032882df32b70ef684fd7460f7
SAM RILEY <samriley1217@gmail.com>
2016-11-09 21:01:16 -0500

    cleaned up my buttons

--- js/redo.js
+++ js/redo.js
@@ -1,38 +1,17 @@
+startGuy = document.getElementById('start')
 var name = 'Joe'
 function runsLater() {
     alert(name)
 }
-//runsLater()
-// Run code 5 seconds in the future
-// var firstTimeout = setTimeout(runsLater, 5000)
-// console.log(firstTimeout)
-//
-// var secondTimeout = setTimeout(function() {
-//     alert('This comes later.')
-// }, 5000)
-// console.log(secondTimeout)
 
-// Stop timeouts from happening
-// clearTimeout(firstTimeout)
-// clearTimeout(secondTimeout)
-
-// Timer button example
-// var counter = 0
-// function doThis() {
-//     counter++
-//     console.log(counter)
-// }
-//
-// var timer
-// function start() {
-//     timer = setInterval(doThis, 1000)
-// }
 counter = 0
+
 var display = document.getElementById('display')
+
 function startClock() {     //<----INCREASES THE COUNTER WITHIN MY OTHER FUNCTIONS
    display.innerHTML = counter //<---PUSHES OUT THE COUNTER WITHIN EACH FUNCTION TO MY HTML PAGE
     counter++
-    //console.log(counter)
+    console.log(counter)
 
 }
 
@@ -40,14 +19,15 @@ var timer
 var doThis
 
 var start = document.getElementById('start').addEventListener('click', startFunction)
+
 function startFunction() {    //<---EVENT CONNECTED TO MY START BUTTON
     clearTimeout(doThis)
     clearInterval(timer)
     timer = setInterval(startClock, 1000)  //<---USES MY STARTCLOCK FUNCTION TO START THE CLOCK
     //console.log(timer)
 }
-var pause = document.getElementById('pause').addEventListener('click', pauseFunction)
-document.getElementById('pause').addEventListener('dblclick', resetFunction) //<---DOUBLE CLICK TO RESET COUNTER
+var pause = document.getElementById('start').addEventListener('click', pauseFunction)
+document.getElementById('start').addEventListener('dblclick', resetFunction) //<---DOUBLE CLICK TO RESET COUNTER
 function pauseFunction() {   //<----EVENT CONNECTED TO MY PAUSE BUTTON
     clearInterval(timer)
     doThis = setTimeout(reset, 5000)
@@ -59,14 +39,17 @@ function resetFunction() {  //<---EVENT CONNECTED TO MY RESET BUTTON
     counter = 0    //<--RESETS COUNTER TO ZERO
     display.innerHTML = counter   //<----LOGS MY NEW COUNTER (0) WHERE THE STARTFUNCTION BEGAN
     //console.log(timer)
+   startGuy.innerHTML = 'Start'
 }
 document.getElementById('start').addEventListener('click', function(){
    //clearInterval(pauseFunction)
-   if (this.innerHTML === 'Pause') {
-      this.innerHTML = 'Continue'
+   if (this.innerHTML === 'Start' || this.innerHTML === 'Continue') {
+      this.innerHTML = 'Pause'
+      startFunction()
    }
    else {
-      this.innerHTML = 'Pause'
+      pauseFunction()
+      this.innerHTML = 'Continue'
 
    }
 })
